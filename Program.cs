8ab530cb721eda92dccedc374ea4f246369e779d
Thomas Gingele <77125551+B1TC0R3@users.noreply.github.com>
2024-07-02 13:45:21 +0200

    Minor refactoring

--- Program.cs
+++ Program.cs
@@ -4,21 +4,12 @@ namespace DailyProgress;
 
 static class Program
 {
+    static readonly DailyProgressForm form = new();
 
-    static DailyProgressForm form = new DailyProgressForm();
-
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
         Application.Run(form);
     }    
-
-    static void SpawnForm() {
-    }
 }
